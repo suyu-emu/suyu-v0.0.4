95d778679d69441fab175932c07b99e1ea2b22c7
crueter <swurl@swurl.xyz>
2025-06-28 23:13:20 -0400

    [ci] fix windows environment variables
    Signed-off-by: crueter <swurl@swurl.xyz>

---
+++ .ci/windows/qt-envvars.sh
@@ -0,0 +1,7 @@
+# This is specific to the CI runner and is unlikely to work on your machine.
+QTDIR="/c/Qt/6.9.0/msvc2022_64"
+
+export QT_ROOT_DIR="$QTDIR"
+export QT_PLUGIN_PATH="$QTDIR/plugins"
+export QML2_IMPORT_PATH="$QTDIR/qml"
+export PATH="${PATH}:$QTDIR/bin"
