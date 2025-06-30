c68c0386ea0c7b44422dfd3379ed297c50841f9c
crueter <swurl@swurl.xyz>
2025-06-29 20:28:01 -0400

    remove pdb files
    Signed-off-by: crueter <swurl@swurl.xyz>

--- .ci/windows/build.sh
+++ .ci/windows/build.sh
@@ -42,7 +42,7 @@ cmake .. -G Ninja \
 ninja
 
 set +e
-find bin -type f -name "*.pdb" -exec rm -fv {} + \; || true
+rm -f bin/*.pdb
 set -e
 
 $WINDEPLOYQT --release --no-compiler-runtime --no-opengl-sw --no-system-dxc-compiler --no-system-d3d-compiler --dir pkg bin/eden.exe
