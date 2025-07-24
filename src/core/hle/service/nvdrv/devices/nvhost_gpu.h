fb0ed5a49c1a913fd8c37c0a348e7ef4f50ed049
SDK-Chan <sdkchan@eden-emu.dev>
2025-07-24 16:57:53 +0200

    [nvdrv] Add AllocGPFIFOEX1 + modify GPFIFOEXs (#115)
    I noticed that AllocGPFIFOEX was missing from eden.
    It is the same as GPFIFOEX2, but is called separately, even on the real console.
    I updated the struct to match closely the one seen on switchbrew, and changed the STUBS, because according to sources they seem to be complete.
    My guess is that the STUBS were remained due to the unknowns back then.
    
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/115
    Co-authored-by: SDK-Chan <sdkchan@eden-emu.dev>
    Co-committed-by: SDK-Chan <sdkchan@eden-emu.dev>

--- src/core/hle/service/nvdrv/devices/nvhost_gpu.h
+++ src/core/hle/service/nvdrv/devices/nvhost_gpu.h
@@ -131,27 +131,13 @@ private:
     static_assert(sizeof(NvFence) == 8, "Fence is incorrect size");
 
     struct IoctlAllocGpfifoEx {
-        u32_le num_entries{};
-        u32_le flags{};
-        u32_le unk0{};
-        u32_le unk1{};
-        u32_le unk2{};
-        u32_le unk3{};
-        u32_le unk4{};
-        u32_le unk5{};
-    };
-    static_assert(sizeof(IoctlAllocGpfifoEx) == 32, "IoctlAllocGpfifoEx is incorrect size");
-
-    struct IoctlAllocGpfifoEx2 {
         u32_le num_entries{}; // in
+        u32_le num_jobs{};    // in
         u32_le flags{};       // in
-        u32_le unk0{};        // in (1 works)
         NvFence fence_out{};  // out
-        u32_le unk1{};        // in
-        u32_le unk2{};        // in
-        u32_le unk3{};        // in
+        std::array<u32_le, 3> reserved{};   // in and ingored (for now) according to switch brew
     };
-    static_assert(sizeof(IoctlAllocGpfifoEx2) == 32, "IoctlAllocGpfifoEx2 is incorrect size");
+    static_assert(sizeof(IoctlAllocGpfifoEx) == 32, "IoctlAllocGpfifoEx2 is incorrect size");
 
     struct IoctlAllocObjCtx {
         u32_le class_num{}; // 0x902D=2d, 0xB197=3d, 0xB1C0=compute, 0xA140=kepler, 0xB0B5=DMA,
@@ -196,7 +182,8 @@ private:
     NvResult ZCullBind(IoctlZCullBind& params);
     NvResult SetErrorNotifier(IoctlSetErrorNotifier& params);
     NvResult SetChannelPriority(IoctlChannelSetPriority& params);
-    NvResult AllocGPFIFOEx2(IoctlAllocGpfifoEx2& params, DeviceFD fd);
+    NvResult AllocGPFIFOEx(IoctlAllocGpfifoEx& params, DeviceFD fd);
+    NvResult AllocGPFIFOEx2(IoctlAllocGpfifoEx& params, DeviceFD fd);
     NvResult AllocateObjectContext(IoctlAllocObjCtx& params);
 
     NvResult SubmitGPFIFOImpl(IoctlSubmitGpfifo& params, Tegra::CommandList&& entries);
