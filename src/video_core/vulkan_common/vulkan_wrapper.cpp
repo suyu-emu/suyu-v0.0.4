7d8ed3fee08dbce1b38d216910be07a1d1d43782
Maufeat <maufeat@eden-emu.dev>
2025-07-28 01:10:16 +0200

    [vk] revert pApplicationName (#144)
    Reverts Vulkan pApplicationName to fix rdna3 lines appearing (tested on totk gloom)
    
    Co-authored-by: Maufeat <sahyno1996@gmail.com>
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/144
    Co-authored-by: Maufeat <maufeat@eden-emu.dev>
    Co-committed-by: Maufeat <maufeat@eden-emu.dev>

--- src/video_core/vulkan_common/vulkan_wrapper.cpp
+++ src/video_core/vulkan_common/vulkan_wrapper.cpp
@@ -444,7 +444,7 @@ Instance Instance::Create(u32 version, Span<const char*> layers, Span<const char
     const VkApplicationInfo application_info{
         .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
         .pNext = nullptr,
-        .pApplicationName = "Eden Emulator",
+        .pApplicationName = "yuzu Emulator",
         .applicationVersion = VK_MAKE_VERSION(0, 1, 0),
         .pEngineName = "Eden Emulator",
         .engineVersion = VK_MAKE_VERSION(0, 1, 0),
