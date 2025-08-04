ff9cf552dca12444f2c535330e9a139c9009182a
lizzie <lizzie@eden-emu.dev>
2025-08-04 18:42:05 +0200

    [dist] update icons + script to update icons (#198)
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/198
    Reviewed-by: crueter <crueter@eden-emu.dev>
    Co-authored-by: lizzie <lizzie@eden-emu.dev>
    Co-committed-by: lizzie <lizzie@eden-emu.dev>

---
+++ .ci/update-icons.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+# SPDX-FileCopyrightText: 2025 eden Emulator Project
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+which png2icns || [ which yay && yay libicns ] || exit
+which magick || exit
+
+export EDEN_SVG_ICO="dist/org.eden_emu.eden.svg"
+svgo --multipass $EDEN_SVG_ICO
+
+magick -density 256x256 -background transparent $EDEN_SVG_ICO \
+    -define icon:auto-resize -colors 256 dist/eden.ico || exit
+magick -size 256x256 -density 256x256 -background transparent $EDEN_SVG_ICO \
+    -define icon:auto-resize -colors 256 dist/yuzu.bmp || exit
+
+export TMP_PNG="dist/eden-tmp.png"
+magick -size 1024x1024 -background transparent $EDEN_SVG_ICO $TMP_PNG || exit
+png2icns dist/eden.icns $TMP_PNG
+cp dist/eden.icns dist/yuzu.icns
+rm $TMP_PNG
