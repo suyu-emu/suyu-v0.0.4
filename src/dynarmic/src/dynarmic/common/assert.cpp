7573213b4538776a99cd506a9ee604867ebe6014
lizzie <lizzie@eden-emu.dev>
2025-08-25 14:43:50 +0200

    [compat] fix clang std::terminate error (#307)
    Signed-off-by: lizzie <lizzie@eden-emu.dev>
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/307
    Reviewed-by: crueter <crueter@eden-emu.dev>
    Co-authored-by: lizzie <lizzie@eden-emu.dev>
    Co-committed-by: lizzie <lizzie@eden-emu.dev>

--- src/dynarmic/src/dynarmic/common/assert.cpp
+++ src/dynarmic/src/dynarmic/common/assert.cpp
@@ -4,6 +4,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <fmt/format.h>
+#include <cstdio>
+#include <exception>
 
 [[noreturn]] void assert_terminate_impl(const char* expr_str, fmt::string_view msg, fmt::format_args args) {
     fmt::print(stderr, "assertion failed: {}\n", expr_str);
