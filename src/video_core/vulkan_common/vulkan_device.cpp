3d53f2381bdad354bd28759c39c7ff930a1fdcbc
JPikachu <jpikachu@eden-emu.dev>
2025-08-27 22:19:17 +0200

    [VK] Refine VRAM allocation strategy for improved stability and performance (#334)
    These adjustments enhance memory management,
    While increasing shader performance across all GPU types, including iGPUs.
    This commit fixes a bug in Super Mario Odyssey where loading into a new area or pausing the game
    Would cause the whole game to slow down (Most noticeable on RDNA 2 GPUs like the Steam Deck)
    
    Thank you to all of our testers for helping eliminate this bug,
    And thank you to Camille for the instructions/commit and to Zephyron for addressing this in Citron.
    
    Co-authored-by: JPikachu <jpikachu.eden@gmail.com>
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/334
    Reviewed-by: CamilleLaVey <camillelavey99@gmail.com>
    Co-authored-by: JPikachu <jpikachu@eden-emu.dev>
    Co-committed-by: JPikachu <jpikachu@eden-emu.dev>

--- src/video_core/vulkan_common/vulkan_device.cpp
+++ src/video_core/vulkan_common/vulkan_device.cpp
@@ -1378,13 +1378,13 @@ void Device::CollectPhysicalMemoryInfo() {
         device_access_memory += mem_properties.memoryHeaps[element].size;
     }
     if (!is_integrated) {
-        const u64 reserve_memory = std::min<u64>(device_access_memory / 8, 1_GiB);
+        const u64 reserve_memory = std::min<u64>(device_access_memory / 4, 2_GiB);
         device_access_memory -= reserve_memory;
 
         if (Settings::values.vram_usage_mode.GetValue() != Settings::VramUsageMode::Aggressive) {
             // Account for resolution scaling in memory limits
-            const size_t normal_memory = 6_GiB;
-            const size_t scaler_memory = 1_GiB * Settings::values.resolution_info.ScaleUp(1);
+            const size_t normal_memory = 8_GiB;
+            const size_t scaler_memory = 2_GiB * Settings::values.resolution_info.ScaleUp(1);
             device_access_memory =
                 std::min<u64>(device_access_memory, normal_memory + scaler_memory);
         }
@@ -1393,7 +1393,7 @@ void Device::CollectPhysicalMemoryInfo() {
     }
     const s64 available_memory = static_cast<s64>(device_access_memory - device_initial_usage);
     device_access_memory = static_cast<u64>(std::max<s64>(
-        std::min<s64>(available_memory - 8_GiB, 4_GiB), std::min<s64>(local_memory, 4_GiB)));
+        std::min<s64>(available_memory - 4_GiB, 6_GiB), std::min<s64>(local_memory, 6_GiB)));
 }
 
 void Device::CollectToolingInfo() {
