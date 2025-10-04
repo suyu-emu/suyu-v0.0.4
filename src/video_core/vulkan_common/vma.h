4ceb496779bd59015201f8885021130842c9412f
crueter <crueter@eden-emu.dev>
2025-10-05 00:00:52 +0200

    [cmake] fix video_core and tests comp errors on Windows (#2631)
    did not link to video_core thus did not properly propagate the GPUOpen
    target thus failed to find vk_mem_alloc
    
    also msvc sucks
    
    Signed-off-by: crueter <crueter@eden-emu.dev>
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/2631
    Reviewed-by: MaranBr <maranbr@eden-emu.dev>

--- src/video_core/vulkan_common/vma.h
+++ src/video_core/vulkan_common/vma.h
@@ -10,4 +10,12 @@
 #define VMA_STATIC_VULKAN_FUNCTIONS 0
 #define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
 
+#ifdef _MSC_VER
+#pragma warning( push )
+#pragma warning( disable : 4189 )
+#endif
 #include "vk_mem_alloc.h"
+
+#ifdef _MSC_VER
+#pragma warning( pop )
+#endif
