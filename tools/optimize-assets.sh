70f2f4274b1c8cebf0e14d58e3c9ef2953c310c3
lizzie <lizzie@eden-emu.dev>
2025-10-29 13:09:05 +0100

    [tools] fix optimize-assets not being multithreaded due to -print0 (#2887)
    Signed-off-by: lizzie <lizzie@eden-emu.dev>
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/2887
    Reviewed-by: crueter <crueter@eden-emu.dev>
    Reviewed-by: Maufeat <sahyno1996@gmail.com>
    Co-authored-by: lizzie <lizzie@eden-emu.dev>
    Co-committed-by: lizzie <lizzie@eden-emu.dev>

--- tools/optimize-assets.sh
+++ tools/optimize-assets.sh
@@ -2,4 +2,6 @@
 # SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 which optipng || exit
-find . -type f -iname '*.png' -print0 | xargs -0 -P 16 -I {} optipng -o7 {}
+NPROC=$(nproc)
+[ -z "$NPROC" ] && NPROC=8
+find . -type f -iname '*.png' | xargs -P $NPROC -I {} optipng -o7 {}
