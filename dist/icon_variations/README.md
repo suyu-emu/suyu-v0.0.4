a62b06a95ae979a62026b402987310da07f1ffc4
lizzie <lizzie@eden-emu.dev>
2025-11-13 13:27:03 +0100

    [dist] small low-fi version of the icon w/o antialias artifacts (#3006)
    This version of the icon is mainly so the main one doesn't look like it has been compressed like a JPEG when shown on the taskbar :)
    Signed-off-by: lizzie lizzie@eden-emu.dev
    
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/3006
    Reviewed-by: Caio Oliveira <caiooliveirafarias0@gmail.com>
    Reviewed-by: MaranBr <maranbr@eden-emu.dev>
    Co-authored-by: lizzie <lizzie@eden-emu.dev>
    Co-committed-by: lizzie <lizzie@eden-emu.dev>

---
+++ dist/icon_variations/README.md
@@ -0,0 +1,9 @@
+# Icon variations
+
+These icons are licensed under GPLv3. Please see the [script for generating icons](../../tools/README.md) and appropriatedly redirect for seasonal icons.
+
+- `base_named.svg` - Named variant.
+- `base_small.svg`: Variant used for tiny icons (16x16, 64x64, etc).
+- `base.svg`: Variant without branding/naming.
+
+Try to keep the icons simple. And preferably automatically be able to be generated (to reduce maintanaince burden). Additionally keep the files small (use `svgo` and `optipng`) to not clutter the git.
--- tools/update-icons.sh
+++ tools/update-icons.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
 # SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
 # SPDX-License-Identifier: GPL-3.0-or-later
@@ -8,20 +8,21 @@
 #which png2icns || (which yay && yay libicns) || exit
 which magick || exit
 
-EDEN_SVG_ICO="dist/dev.eden_emu.eden.svg"
-EALT_SVG_ICO="dist/eden_named.svg"
+EDEN_BASE_SVG="dist/icon_variations/base.svg"
+EDEN_SMALL_SVG="dist/icon_variations/base_small.svg"
+EDEN_NAMED_SVG="dist/icon_variations/base_named.svg"
 
-magick -density 256x256 -background transparent $EDEN_SVG_ICO -define icon:auto-resize -colors 256 dist/eden.ico || exit
-convert -density 256x256 -resize 256x256 -background transparent $EDEN_SVG_ICO dist/yuzu.bmp || exit
+magick -density 256x256 -background transparent $EDEN_SMALL_SVG -define icon:auto-resize -colors 256 dist/eden.ico || exit
+convert -density 256x256 -resize 256x256 -background transparent $EDEN_SMALL_SVG dist/yuzu.bmp || exit
 
-magick -size 256x256 -background transparent $EDEN_SVG_ICO dist/qt_themes/default/icons/256x256/eden.png || exit
-magick -size 256x256 -background transparent $EALT_SVG_ICO dist/qt_themes/default/icons/256x256/eden_named.png || exit
+magick -size 256x256 -background transparent $EDEN_BASE_SVG dist/qt_themes/default/icons/256x256/eden.png || exit
+magick -size 256x256 -background transparent $EDEN_NAMED_SVG dist/qt_themes/default/icons/256x256/eden_named.png || exit
 magick dist/qt_themes/default/icons/256x256/eden.png -resize 256x256! dist/qt_themes/default/icons/256x256/eden.png || exit
 magick dist/qt_themes/default/icons/256x256/eden_named.png -resize 256x256! dist/qt_themes/default/icons/256x256/eden_named.png || exit
 
 # Now do more fancy things (like composition)
 TMP_PNG="dist/eden-tmp.png"
-magick -size 1024x1024 -background transparent $EDEN_SVG_ICO $TMP_PNG || exit
+magick -size 1024x1024 -background transparent $EDEN_BASE_SVG $TMP_PNG || exit
 composite $TMP_PNG -gravity center -geometry 2048x2048+0+0 \
     src/android/app/src/main/res/drawable/ic_icon_bg_orig.png \
     src/android/app/src/main/res/drawable/ic_launcher.png || exit
@@ -31,6 +32,6 @@ optipng -o7 src/android/app/src/main/res/drawable/ic_launcher.png
 optipng -o7 dist/qt_themes/default/icons/256x256/eden_named.png
 optipng -o7 dist/qt_themes/default/icons/256x256/eden.png
 
-png2icns dist/eden.icns $TMP_PNG
+png2icns dist/eden.icns $TMP_PNG || echo 'non fatal'
 cp dist/eden.icns dist/yuzu.icns
 rm $TMP_PNG
