8311a8a98fed163243b4e0a3f90162aed802c1de
lizzie <lizzie@eden-emu.dev>
2025-11-17 22:50:05 +0100

    [dynarmic] force-disable perfmap on non-platforms (#3031)
    removes some annoying generated stubs with just a `retq`...
    
    Signed-off-by: lizzie lizzie@eden-emu.dev
    
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/3031
    Reviewed-by: Caio Oliveira <caiooliveirafarias0@gmail.com>
    Reviewed-by: crueter <crueter@eden-emu.dev>
    Co-authored-by: lizzie <lizzie@eden-emu.dev>
    Co-committed-by: lizzie <lizzie@eden-emu.dev>

--- src/dynarmic/src/dynarmic/backend/x64/perf_map.h
+++ src/dynarmic/src/dynarmic/backend/x64/perf_map.h
@@ -14,15 +14,19 @@
 
 namespace Dynarmic::Backend::X64 {
 
+#if defined(__linux__) && !defined(__ANDROID__)
 namespace detail {
 void PerfMapRegister(const void* start, const void* end, std::string_view friendly_name);
 }  // namespace detail
-
 template<typename T>
-void PerfMapRegister(T start, const void* end, std::string_view friendly_name) {
+void PerfMapRegister(T start, const void* end, std::string_view friendly_name) noexcept {
     detail::PerfMapRegister(std::bit_cast<const void*>(start), end, friendly_name);
 }
-
 void PerfMapClear();
+#else
+// Resolve to no-op (compiler thinks fmt has side effects)
+template<typename T> inline void PerfMapRegister(T, const void*, std::string_view) noexcept {}
+inline void PerfMapClear() noexcept {}
+#endif
 
 }  // namespace Dynarmic::Backend::X64
