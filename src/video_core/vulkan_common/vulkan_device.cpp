dedbdbb074cd5b1bbf98a31a2c3a20ee7c6c31e9
lizzie <lizzie@eden-emu.dev>
2025-12-04 07:30:59 +0100

    [vk] attempt to continue even if unsuitable driver (#3087)
    rationale:
    - some drivers will not outright crash (and keeping a list of those who don't crash is tedious)
    - if it does crash we can get a log line saying "hey, this driver? unsuitable"
    - makes lfie with lavapipe a tad bit easier
    Signed-off-by: lizzie lizzie@eden-emu.dev
    
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/3087
    Reviewed-by: crueter <crueter@eden-emu.dev>
    Reviewed-by: CamilleLaVey <camillelavey99@gmail.com>
    Co-authored-by: lizzie <lizzie@eden-emu.dev>
    Co-committed-by: lizzie <lizzie@eden-emu.dev>

--- src/video_core/vulkan_common/vulkan_device.cpp
+++ src/video_core/vulkan_common/vulkan_device.cpp
@@ -428,13 +428,10 @@ Device::Device(VkInstance instance_, vk::PhysicalDevice physical_, VkSurfaceKHR 
     const bool is_qualcomm = driver_id == VK_DRIVER_ID_QUALCOMM_PROPRIETARY;
     const bool is_turnip = driver_id == VK_DRIVER_ID_MESA_TURNIP;
     const bool is_s8gen2 = device_id == 0x43050a01;
-    const bool is_arm = driver_id == VK_DRIVER_ID_ARM_PROPRIETARY;
+    //const bool is_arm = driver_id == VK_DRIVER_ID_ARM_PROPRIETARY;
 
-    if ((is_mvk || is_qualcomm || is_turnip || is_arm) && !is_suitable) {
-        LOG_WARNING(Render_Vulkan, "Unsuitable driver, continuing anyway");
-    } else if (!is_suitable) {
-        throw vk::Exception(VK_ERROR_INCOMPATIBLE_DRIVER);
-    }
+    if (!is_suitable)
+        LOG_WARNING(Render_Vulkan, "Unsuitable driver - continuing anyways");
 
     if (is_nvidia) {
         nvidia_arch = GetNvidiaArchitecture(physical, supported_extensions);
