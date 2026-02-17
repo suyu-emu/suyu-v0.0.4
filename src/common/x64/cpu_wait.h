660da9c5b526e759d631a9205c3d8340dff52062
John <john@eden-emu.dev>
2026-02-17 06:15:45 +0100

    [windows] Return x86 microsleep for Windows (#3563)
    Microsleep removal may have regressed AOC in msvc
    
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/3563
    Reviewed-by: CamilleLaVey <camillelavey99@gmail.com>
    Reviewed-by: Lizzie <lizzie@eden-emu.dev>
    Co-authored-by: John <john@eden-emu.dev>
    Co-committed-by: John <john@eden-emu.dev>

---
+++ src/common/x64/cpu_wait.h
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+namespace Common::X64 {
+
+void MicroSleep();
+
+} // namespace Common::X64
