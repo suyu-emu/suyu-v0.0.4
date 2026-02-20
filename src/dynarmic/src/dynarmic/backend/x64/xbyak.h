dd1125929f0de2a06e639fa4cc4cb20ab00dcec0
lizzie <lizzie@eden-emu.dev>
2026-02-20 19:28:36 +0100

    [xbyak] fix unordered map that wasn't actually wanting to be unordered (#3589)
    It seems to be related to the fact multimap on xbyak doesn't properly work if it's unordered
    It also seems to NOT play nice at all with unstable iterators (like those found on ankerl) - either we use boost or just let xbyak use normal std
    
    Signed-off-by: lizzie <lizzie@eden-emu.dev>
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/3589
    Reviewed-by: Maufeat <sahyno1996@gmail.com>
    Reviewed-by: crueter <crueter@eden-emu.dev>
    Co-authored-by: lizzie <lizzie@eden-emu.dev>
    Co-committed-by: lizzie <lizzie@eden-emu.dev>

--- src/dynarmic/src/dynarmic/backend/x64/xbyak.h
+++ src/dynarmic/src/dynarmic/backend/x64/xbyak.h
@@ -3,12 +3,13 @@
 
 #pragma once
 
-#define XBYAK_STD_UNORDERED_SET ankerl::unordered_dense::set
-#define XBYAK_STD_UNORDERED_MAP ankerl::unordered_dense::map
-#define XBYAK_STD_UNORDERED_MULTIMAP boost::unordered_multimap
+#include <unordered_map>
+#include <unordered_set>
 
-#include <boost/unordered_map.hpp>
-#include <ankerl/unordered_dense.h>
+// TODO: Defining this crashes e v e r y t h i n g
+// #define XBYAK_STD_UNORDERED_SET ankerl::unordered_dense::set
+// #define XBYAK_STD_UNORDERED_MAP ankerl::unordered_dense::map
+// #define XBYAK_STD_UNORDERED_MULTIMAP boost::unordered_multimap
 
 #include <xbyak/xbyak.h>
 #include <xbyak/xbyak_util.h>
