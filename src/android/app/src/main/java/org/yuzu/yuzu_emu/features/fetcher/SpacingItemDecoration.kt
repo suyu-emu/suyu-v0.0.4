51289dda0a7a990b4ecb365c853b0671bb4d0fde
xbzk <xbzk@eden-emu.dev>
2026-03-05 13:58:46 +0100

    [android,ui] fixed top disalignment between buttons of each column in settings fragment (#3675)
    this silly little thing tickles obsessive compulsive disturbed fellas a lot hu3
    was shipped along PR 3660, which was rediscussed for other reason, hence this tiny lonely PR.
    
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/3675
    Reviewed-by: DraVee <chimera@dravee.dev>
    Reviewed-by: MaranBr <maranbr@eden-emu.dev>
    Co-authored-by: xbzk <xbzk@eden-emu.dev>
    Co-committed-by: xbzk <xbzk@eden-emu.dev>

--- src/android/app/src/main/java/org/yuzu/yuzu_emu/features/fetcher/SpacingItemDecoration.kt
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/features/fetcher/SpacingItemDecoration.kt
@@ -1,10 +1,11 @@
-// SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 package org.yuzu.yuzu_emu.features.fetcher
 
 import android.graphics.Rect
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class SpacingItemDecoration(private val spacing: Int) : RecyclerView.ItemDecoration() {
@@ -15,8 +16,20 @@ class SpacingItemDecoration(private val spacing: Int) : RecyclerView.ItemDecorat
         state: RecyclerView.State
     ) {
         outRect.bottom = spacing
-        if (parent.getChildAdapterPosition(view) == 0) {
+
+        val position = parent.getChildAdapterPosition(view)
+        if (position == RecyclerView.NO_POSITION) return
+
+        if (position == 0) {
             outRect.top = spacing
+            return
+        }
+
+        // If the item is in the first row, but NOT in first column add top spacing as well
+        val layoutManager = parent.layoutManager
+        if (layoutManager is GridLayoutManager && layoutManager.spanSizeLookup.getSpanGroupIndex(position, layoutManager.spanCount) == 0) {
+            outRect.top = spacing
+            return
         }
     }
 }
