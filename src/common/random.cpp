5e8575ad598014287eb295051726a6fd2e5f3657
lizzie <lizzie@eden-emu.dev>
2026-03-31 23:45:06 +0200

    [android] fix crash due to ctor/dtor ordering for std::random_device being uninitialized when used in other static ctor/dtors (#3806)
    wow that's fucking horrible
    
    Signed-off-by: lizzie <lizzie@eden-emu.dev>
    
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/3806
    Reviewed-by: MaranBr <maranbr@eden-emu.dev>
    Reviewed-by: CamilleLaVey <camillelavey99@gmail.com>
    Co-authored-by: lizzie <lizzie@eden-emu.dev>
    Co-committed-by: lizzie <lizzie@eden-emu.dev>

--- src/common/random.cpp
+++ src/common/random.cpp
@@ -1,19 +1,22 @@
 // SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include <optional>
 #include <random>
 #include "common/random.h"
 
-static std::random_device g_random_device;
-
 namespace Common::Random {
+    [[nodiscard]] static std::random_device& GetGlobalRandomDevice() noexcept {
+        static std::random_device g_random_device{};
+        return g_random_device;
+    }
     [[nodiscard]] u32 Random32(u32 seed) noexcept {
-        return g_random_device();
+        return GetGlobalRandomDevice()();
     }
     [[nodiscard]] u64 Random64(u64 seed) noexcept {
-        return g_random_device();
+        return GetGlobalRandomDevice()();
     }
     [[nodiscard]] std::mt19937 GetMT19937() noexcept {
-        return std::mt19937(g_random_device());
+        return std::mt19937(GetGlobalRandomDevice()());
     }
 }
