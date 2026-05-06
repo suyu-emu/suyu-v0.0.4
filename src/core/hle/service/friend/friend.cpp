797e36b6db382537b628c526729bacbbdd6e45da
crueter <crueter@eden-emu.dev>
2026-05-06 20:23:42 +0200

    [hle, friend] GetFriendListForViewerV2 STUBBED and update list of functions (#3933) (#3937)
    Fixes freezes Friend List on firmware 22+ and update list of functions according to data from Switchbrew
    
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/3933
    Reviewed-by: crueter <crueter@eden-emu.dev>
    Reviewed-by: MaranBr <maranbr@eden-emu.dev>
    
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/3937

--- src/core/hle/service/friend/friend.cpp
+++ src/core/hle/service/friend/friend.cpp
@@ -53,38 +53,53 @@ public:
             {20102, nullptr, "GetFriendDetailedInfo"},
             {20103, nullptr, "SyncFriendList"},
             {20104, &IFriendService::RequestSyncFriendList, "RequestSyncFriendList"},
-            {20105, &IFriendService::GetFriendListForViewer, "GetFriendListForViewer"},
-            {20110, nullptr, "LoadFriendSetting"},
+            {20105, &IFriendService::GetFriendListForViewer, "GetFriendListForViewerV1"},
+            {20106, nullptr, "UpdateFriendInfoForViewerV1"},
+            {20107, nullptr, "GetFriendDetailedInfoV2"}, // 20.0.0+
+            {20108, &IFriendService::GetFriendListForViewer, "GetFriendListForViewerV2"}, // 22.0.0+
+            {20109, nullptr, "UpdateFriendInfoForViewerV2"}, // 22.0.0+
+            {20110, nullptr, "LoadFriendSettingV1"},
+            {20111, nullptr, "LoadFriendSettingV2"}, // 22.0.0+
             {20200, &IFriendService::GetReceivedFriendRequestCount, "GetReceivedFriendRequestCount"},
-            {20201, nullptr, "GetFriendRequestList"},
+            {20201, nullptr, "GetFriendRequestListV1"},
+            {20202, nullptr, "GetFriendRequestListV2"}, // 20.0.0+
+            {20203, nullptr, "GetFriendRequestReceivedNotificationCount"}, // 22.0.0+
             {20300, nullptr, "GetFriendCandidateList"},
             {20301, nullptr, "GetNintendoNetworkIdInfo"},
-            {20302, nullptr, "GetSnsAccountLinkage"},
-            {20303, nullptr, "GetSnsAccountProfile"},
-            {20304, nullptr, "GetSnsAccountFriendList"},
-            {20400, nullptr, "GetBlockedUserList"},
+            {20302, nullptr, "GetSnsAccountLinkage"}, // 5.0.0-19.0.1
+            {20303, nullptr, "GetSnsAccountProfile"}, // 5.0.0-19.0.1
+            {20304, nullptr, "GetSnsAccountFriendList"}, // 5.0.0-19.0.1
+            {20400, nullptr, "GetBlockedUserListV1"},
             {20401, nullptr, "SyncBlockedUserList"},
-            {20500, nullptr, "GetProfileExtraList"},
+            {20402, nullptr, "GetBlockedUserListV2"}, // 20.0.0+
+            {20500, nullptr, "GetProfileExtraListV1"},
             {20501, nullptr, "GetRelationship"},
+            {20502, nullptr, "GetProfileExtraListV2"}, // 19.0.0+
             {20600, &IFriendService::GetUserPresenceView, "GetUserPresenceViewV1"},
-            {20601, &IFriendService::GetUserPresenceView, "GetUserPresenceViewV2"},
-            {20700, nullptr, "GetPlayHistoryList"},
+            {20601, &IFriendService::GetUserPresenceView, "GetUserPresenceViewV2"}, // 19.0.0+
+            {20700, nullptr, "GetPlayHistoryListV1"},
             {20701, &IFriendService::GetPlayHistoryStatistics, "GetPlayHistoryStatistics"},
+            {20702, nullptr, "GetPlayHistoryListV2"}, // 19.0.0+
             {20800, &IFriendService::LoadUserSetting, "LoadUserSettingV1"},
             {20801, nullptr, "SyncUserSetting"},
-            {20802, &IFriendService::LoadUserSetting, "LoadUserSettingV2"},
+            {20802, &IFriendService::LoadUserSetting, "LoadUserSettingV2"}, // 19.0.0+
             {20900, &IFriendService::RequestListSummaryOverlayNotification, "RequestListSummaryOverlayNotification"},
             {21000, nullptr, "GetExternalApplicationCatalog"},
-            {22000, nullptr, "GetReceivedFriendInvitationList"},
-            {22001, nullptr, "GetReceivedFriendInvitationDetailedInfo"},
+            {22000, nullptr, "GetReceivedFriendInvitationListV1"},
+            {22001, nullptr, "GetReceivedFriendInvitationDetailedInfoV1"},
+            {22002, nullptr, "GetReceivedFriendInvitationListV2"}, // 19.0.0+
+            {22003, nullptr, "GetReceivedFriendInvitationDetailedInfoV2"}, // 19.0.0+
             {22010, &IFriendService::GetReceivedFriendInvitationCountCache, "GetReceivedFriendInvitationCountCache"},
             {30100, nullptr, "DropFriendNewlyFlags"},
             {30101, nullptr, "DeleteFriend"},
             {30110, nullptr, "DropFriendNewlyFlag"},
             {30120, nullptr, "ChangeFriendFavoriteFlag"},
             {30121, nullptr, "ChangeFriendOnlineNotificationFlag"},
+            {30130, nullptr, "SetFriendNote"}, // 22.0.0+
+            {30131, nullptr, "RequestUploadPendingNote"}, // 22.0.0+
+            {30190, nullptr, "RequestSyncLocalUpdates"}, // 22.0.0+
             {30200, nullptr, "SendFriendRequest"},
-            {30201, nullptr, "SendFriendRequestWithApplicationInfo"},
+            {30201, nullptr, "SendFriendRequestWithApplicationInfoV1"},
             {30202, nullptr, "CancelFriendRequest"},
             {30203, nullptr, "AcceptFriendRequest"},
             {30204, nullptr, "RejectFriendRequest"},
@@ -97,21 +112,27 @@ public:
             {30215, nullptr, "SendFriendRequestWithExternalApplicationCatalogId"},
             {30216, nullptr, "ResendFacedFriendRequest"},
             {30217, nullptr, "SendFriendRequestWithNintendoNetworkIdInfo"},
-            {30300, nullptr, "GetSnsAccountLinkPageUrl"},
-            {30301, nullptr, "UnlinkSnsAccount"},
+            {30218, nullptr, "SendFriendRequestWithApplicationInfoV2"}, // 20.0.0+
+            {30300, nullptr, "GetSnsAccountLinkPageUrl"}, // 5.0.0-19.0.1
+            {30301, nullptr, "UnlinkSnsAccount"}, // 5.0.0-19.0.1
             {30400, nullptr, "BlockUser"},
-            {30401, nullptr, "BlockUserWithApplicationInfo"},
+            {30401, nullptr, "BlockUserWithApplicationInfoV1"},
             {30402, nullptr, "UnblockUser"},
-            {30500, nullptr, "GetProfileExtraFromFriendCode"},
+            {30403, nullptr, "BlockUserWithApplicationInfoV2"}, // 20.0.0+
+            {30500, nullptr, "GetProfileExtraFromFriendCodeV1"},
+            {30501, nullptr, "GetProfileExtraFromFriendCodeV2"}, // 19.0.0+
             {30700, nullptr, "DeletePlayHistory"},
+            {30701, nullptr, "AddPlayHistoryWithApplication"}, // 19.0.0+
             {30810, nullptr, "ChangePresencePermission"},
             {30811, nullptr, "ChangeFriendRequestReception"},
             {30812, nullptr, "ChangePlayLogPermission"},
             {30820, nullptr, "IssueFriendCode"},
             {30830, nullptr, "ClearPlayLog"},
-            {30900, nullptr, "SendFriendInvitation"},
+            {30900, nullptr, "SendFriendInvitationV1"},
+            {30901, nullptr, "SendFriendInvitationV2"}, // 19.0.0+
             {30910, nullptr, "ReadFriendInvitation"},
             {30911, nullptr, "ReadAllFriendInvitations"},
+            {31000, nullptr, "OpenUser"}, // 19.0.0+
             {40100, nullptr, "DeleteFriendListCache"},
             {40400, nullptr, "DeleteBlockedUserListCache"},
             {49900, nullptr, "DeleteNetworkServiceAccountCache"},
