c398bde787422218dd12d77a888dd935d97034e8
lizzie <lizzie@eden-emu.dev>
2026-05-13 19:14:18 +0200

    [vk, ogl] bump shader cache version to 17 (#3947)
    Signed-off-by: lizzie <lizzie@eden-emu.dev>
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/3947
    Reviewed-by: MaranBr <maranbr@eden-emu.dev>
    Reviewed-by: crueter <crueter@eden-emu.dev>

--- src/video_core/renderer_opengl/gl_shader_cache.cpp
+++ src/video_core/renderer_opengl/gl_shader_cache.cpp
@@ -54,7 +54,7 @@ using VideoCommon::LoadPipelines;
 using VideoCommon::SerializePipeline;
 using Context = ShaderContext::Context;
 
-constexpr u32 CACHE_VERSION = 14;
+constexpr u32 CACHE_VERSION = 15;
 
 template <typename Container>
 auto MakeSpan(Container& container) {
